e8781346d960c29197314d400094bdab5fa082c5
gityingjianglin <yingjianglin123@163.com>
2023-03-21 10:27:35 +0800

    更新工具类库

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ import DictTag from '@/components/DictTag'
 import VueMeta from 'vue-meta'
 // 字典数据组件
 import DictData from '@/components/DictData'
+import wlkjUtils from 'wlkj-utils'
 
 // 全局方法挂载
 Vue.prototype.getDicts = getDicts
@@ -48,6 +49,9 @@ Vue.prototype.selectDictLabel = selectDictLabel
 Vue.prototype.selectDictLabels = selectDictLabels
 Vue.prototype.download = download
 Vue.prototype.handleTree = handleTree
+
+Vue.prototype.$wlkjUtils = wlkjUtils
+console.log(wlkjUtils.date.parseTime(1673407269))
 
 // 全局组件挂载
 Vue.component('DictTag', DictTag)
